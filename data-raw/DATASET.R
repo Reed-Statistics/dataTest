e00ea74782491d98b8f012bb0cb8fa65bc270524
mcconvil <mcconville@reed.edu>
2020-02-12 12:13:46 -0800

    Adding the blog post placeholder

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -2,11 +2,17 @@
 
 # Necessary libraries
 library(janitor)
+library(lubridate)
+library(tidyverse)
 
 dat <- read.csv("https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD")
 
 # Clean up variable names
 fremontBikes <- clean_names(dat)
+
+# Fix date
+fremontBikes <- fremontBikes %>%
+  mutate(date = mdy_hms(date))
 
 
 usethis::use_data(fremontBikes, overwrite = TRUE)
